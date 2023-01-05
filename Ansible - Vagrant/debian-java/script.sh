7db99dce8270d51181d1781c2aee735a4fb4d737
Renan Leite Vieira <109419403+renanleitev@users.noreply.github.com>
2023-01-05 14:33:12 -0300

    fix: bypass grub-pc on install

--- Ansible - Vagrant/debian-java/script.sh
+++ Ansible - Vagrant/debian-java/script.sh
@@ -3,22 +3,27 @@
 # Atualizando o repositório do  sistema
 apt update
 
+# Instalando os programas
+yes | sudo DEBIAN_FRONTEND=noninteractive apt-get -yq install \
+xfce4 \
+xfce4-goodies \
+firefox-esr \
+default-jdk \
+ -y
+
 # Instalando o deb-get (para instalar pacotes deb via terminal)
 if [[ $(which deb-get) ]]
 then
   echo "Pacote deb-get já instalado no sistema..."
 else
   echo "Instalando deb-get no sistema..."
-  curl -sL https://raw.githubusercontent.com/wimpysworld/deb-get/main/deb-get | sudo -E bash -s install deb-get
+  wget https://github.com/wimpysworld/deb-get/releases/download/0.3.6/deb-get_0.3.6-1_all.deb
+  sudo DEBIAN_FRONTEND=noninteractive apt-get -yq install /home/vagrant/deb-get_0.3.6-1_all.deb
+  rm deb-get*
 fi
 
-# Instalando os programas
-apt install \
-xfce4 \
-xfce4-goodies \
-firefox-esr \
-default-jdk \
- -y
+# Instalando o Visual Studio Code e Sublime Text
+deb-get install code sublime-text
 
 # Instalando o Netbeans
 if [[ $(apt list --installed | grep "netbeans") ]]
@@ -31,8 +36,27 @@ else
   rm apache*
 fi
 
-# Instalando o Visual Studio Code e Sublime Text
-deb-get install code sublime-text
+# Instalando o Eclipse
+if [[ $(ls /usr/share/applications/ | grep "eclipse*") ]]
+then
+  echo "Eclipse IDE já instalado no sistema..."
+else
+  echo "Instalando Eclipse IDE no sistema..."
+  wget https://mirror.umd.edu/eclipse/technology/epp/downloads/release/2022-12/R/eclipse-java-2022-12-R-linux-gtk-x86_64.tar.gz
+  tar -xzvf eclipse-java-2022-12-R-linux-gtk-x86_64.tar.gz -C /opt
+  rm eclipse-java-2022-12-R-linux-gtk-x86_64.tar.gz
+  ln -sf /opt/eclipse/eclipse /usr/bin/eclipse
+  touch /usr/share/applications/eclipse.desktop
+  echo "[Desktop Entry]
+  Encoding=UTF-8
+  Name=Eclipse IDE
+  Comment=Eclipse IDE for Java Developers
+  Exec=/usr/bin/eclipse
+  Icon=/usr/eclipse/icon.xpm
+  Categories=Application;Development;Java;IDE
+  Type=Application
+  Terminal=0" >> /usr/share/applications/eclipse.desktop
+fi
 
 # Definindo o fuso horário correto (Recife/PE)
 echo "Etc/GMT+3" > /etc/timezone
@@ -40,6 +64,7 @@ ln -fs /usr/share/zoneinfo/`cat /etc/timezone` /etc/localtime
 dpkg-reconfigure -f noninteractive tzdata
 
 # Definindo o teclado para o padrão abnt2
+. /home/vagrant/.bashrc
 if [[ $(cat /home/vagrant/.bashrc | grep "setxkbmap") ]]
 then
   echo "Teclado já configurado para o padrão abnt2..."
