89ba276be5f21d3d0c69ba8be0eda4f5e5cf4596
renanleitev <renanleitev@gmail.com>
2023-01-20 23:30:15 -0300

    fix: exercicio numero-invertido.js

--- Javascript/Exercicios/logica/numero-invertido.js
+++ Javascript/Exercicios/logica/numero-invertido.js
@@ -1,10 +1,10 @@
 function numeroInvertido(num){
-    const numero = Array.from(String(num));
-    const invertido = [];
-    let incremento = 0;
-    for (let num of numero){
-        invertido.shift()
+    const numero = Array.from(num.toString());
+    let invertido = [];
+    for (let i = 0; i < numero.length; i++){
+        invertido[i] = numero[(numero.length-1)-i];
     }
+    invertido = Number.parseInt(invertido.toString().replaceAll(',', ''));
     return invertido;
 }
 console.log(numeroInvertido(875));
--- Javascript/Exercicios/logica/tempCodeRunnerFile.js
+++ Javascript/Exercicios/logica/tempCodeRunnerFile.js
@@ -1,14 +1,3 @@
-function qualMaior(x, y){
-//     if (x > y){
-//         console.log(`${x} é maior que ${y}.`);
-//     }
-//     else if (x < y){
-//         console.log(`${y} é maior que ${x}.`);
-//     }
-//     else if (x === y){
-//         console.log('Os números são iguais.');
-//     }
-//     else{
-//         console.log("Erro. Digite números válidos.")
-//     }
-// }
+    // for (let i = -1; i > ){
+        
+    // }
